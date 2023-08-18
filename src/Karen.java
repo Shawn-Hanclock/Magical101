b0b637ded35c8e7806f78b1d9b52bdeef6aab651
Shawn-Hanclock <nwahs007@gmail.com>
2023-08-18 13:24:42 -0700

    Created the Main and Karen class. Main contains a sout "Hello World" Karen contains two constructors and an instance variable.

---
+++ src/Karen.java
@@ -0,0 +1,15 @@
+public class Karen
+{
+    String name;
+    //instance variables
+
+    public Karen()
+    {//start empty constructor
+        this.name = "Karen";
+    }//end empty constructor
+
+    public Karen(String name)
+    {//start name constructor
+        this.name = name;
+    }//end name constructor
+}
