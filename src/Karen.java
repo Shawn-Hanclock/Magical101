bb5289c7c5bd13433cfe65cf22d6caef4ed44cb9
Shawn-Hanclock <nwahs007@gmail.com>
2023-08-24 13:02:16 -0700

    Created getters and setters for some of the variables in Karen.

--- src/Karen.java
+++ src/Karen.java
@@ -1,15 +1,28 @@
 public class Karen
 {
     String name;
+    int numRacers;
     //instance variables
 
     public Karen()
     {//start empty constructor
         this.name = "Karen";
+        this.numRacers = 0;
     }//end empty constructor
 
     public Karen(String name)
     {//start name constructor
         this.name = name;
+        this.numRacers = 0;
     }//end name constructor
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setNumRacers(int numRacers)
+    {
+        this.numRacers = numRacers;
+    }//setter for numRacers, the number of animals being raced
 }
