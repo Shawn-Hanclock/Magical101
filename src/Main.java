5958f9808c3c2d45c546fc754a36685b695265d7
Shawn-Hanclock <nwahs007@gmail.com>
2023-08-24 13:19:57 -0700

    created takeRacers method, hoping to use recursion for the input validation.

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,23 @@ public class Main
         Karen user = new Karen(input.next());
         System.out.println("Hello " + user.getName() + " we're located here at Magical McDonald's, How many creatures will you race?");
         System.out.println("Just two? Or three? Enter '2' or '3': \n");
-//        if(input.hasNextInt())
-//        {
-//            while (input.nextInt() != 2 || input.nextInt() != 3)
-//            {
-//                System.out.println("Just two? Or three? Enter '2' or '3': \n");
-//            }
-//        }
+        takeRacers(input);
 
-//        user.setNumRacers(input.nextInt());
+        user.setNumRacers(input.nextInt());
 
 
         Dragon bob = new Dragon("bob", "lizard", 9999, 14.272);
         System.out.println(bob.toString());
     }//end of main method
+
+    private static void takeRacers(Scanner value)
+    {
+        while(value.hasNextInt())
+        {
+            if (value.nextInt() != 2 || value.nextInt() != 3)
+            {
+                System.out.println("Just two? Or three? Enter '2' or '3': \n");
+            }
+        }
+    }
 }
