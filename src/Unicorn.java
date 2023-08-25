5493c5772d61c3c81a24915bd57678c6a9046b61
Shawn-Hanclock <nwahs007@gmail.com>
2023-08-25 12:41:44 -0700

    created unicorn class with two constructors

---
+++ src/Unicorn.java
@@ -0,0 +1,11 @@
+public class Unicorn extends MagicalAnimal
+{
+    public Unicorn()
+    {
+        super();
+    }//end of constructor blank
+    public Unicorn(String name, String type, int speed, double weight)
+    {
+        super(name, type, speed, weight);
+    }//end of constructor full
+}
