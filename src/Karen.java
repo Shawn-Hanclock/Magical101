c681cc537cc3bc9b81af3e3b82816a543c83f099
Shawn-Hanclock <nwahs007@gmail.com>
2023-08-31 12:30:09 -0700

    revised contructor to work with the code in main

--- src/Karen.java
+++ src/Karen.java
@@ -1,7 +1,10 @@
+//import java.util.ArrayList;
+//import java.util.Scanner;
+
 public class Karen
 {
-    String name;
-    int numRacers;
+    private String name;
+    private int numRacers;
     //instance variables
 
     public Karen()
@@ -18,11 +21,42 @@ public class Karen
 
     public String getName()
     {
-        return name;
+        return this.name;
     }//getter for Karen name
+
+//    public int getNumRacers()
+//    {
+//        return this.numRacers;
+//    }
 
     public void setNumRacers(int numRacers)
     {
         this.numRacers = numRacers;
     }//setter for numRacers, the number of animals being raced
+
+//    public void createAnimal(String type, ArrayList list)
+//    {
+//        String typeCheck = type;
+//        Scanner input = new Scanner(System.in);
+//        switch (typeCheck)
+//        {
+//            case "d":
+//                System.out.println("What's the name of the dragon?\nEnter your dragon name (eg. 'toothless'):");
+//                Dragon creatureD = new Dragon(input.next());
+//                list.add(creatureD);
+//                break;
+//            case "u":
+//                System.out.println("What's the name of the unicorn?\nEnter your unicorn name (eg. 'rainbows'):");
+//                Unicorn creatureU = new Unicorn(input.next());
+//                list.add(creatureU);
+//                break;
+//            case "g":
+//                break;
+//            default:
+//                System.out.println("Enter from the given options:\n 'd' for dragon\n 'u' for unicorn\n 'g' for griffith");
+//                typeCheck = input.next();
+//        }
+//    }
+
+
 }
