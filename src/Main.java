27df708f5260b225679e9b62297c131d9b0e4007
Shawn-Hanclock <nwahs007@gmail.com>
2023-08-31 13:01:19 -0700

    added the code for creating the magical animal list used in karen class

--- src/Main.java
+++ src/Main.java
@@ -13,20 +13,20 @@ public class Main
         user.setNumRacers(input.nextInt());
         //takes the number of racers
 
-//        ArrayList<MagicalAnimal> creatures = new ArrayList<MagicalAnimal>();
-//        System.out.println("Now lets pick out magical animals.");
-//        System.out.println("What should the first creature be?");
-//        boolean creaturesMade = false;
-//        while(!creaturesMade)
-//        {
-//            if(creatures.size() != 0)
-//                System.out.println("What should the next creature be?");
-//            System.out.println(" 'd' for dragon \n 'u' for unicorn \n 'g' for griffith");
-//            if(input.hasNext())
-//                user.createAnimal(input.next(), creatures);
-//            creaturesMade = creatures.size() == user.getNumRacers();
-//        }
-//        //creates arrayList of our magical animals
-//        System.out.println(creatures.get(0).toString());
+        ArrayList<MagicalAnimal> creatures = new ArrayList<MagicalAnimal>();
+        System.out.println("Now lets pick out magical animals.");
+        System.out.println("What should the first creature be?");
+        boolean creaturesMade = false;
+        while(!creaturesMade)
+        {
+            if(creatures.size() != 0)
+                System.out.println("What should the next creature be?");
+            System.out.println(" 'd' for dragon \n 'u' for unicorn \n 'g' for griffith");
+            if(input.hasNext())
+                user.createAnimal(input.next(), creatures);
+            creaturesMade = creatures.size() == user.getNumRacers();
+        }
+        //creates arrayList of our magical animals
+        System.out.println(creatures.get(0).toString());
      }//end of main method
 }
