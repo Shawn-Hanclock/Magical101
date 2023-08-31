63d224260a3361680bd79080e2b676be7ad13c5c
Shawn-Hanclock <nwahs007@gmail.com>
2023-08-31 12:43:03 -0700

    created the createAnimal method, that takes in an array list that stores the animals and type used to pick the animal being created (represented as a string)

--- src/Karen.java
+++ src/Karen.java
@@ -1,5 +1,5 @@
-//import java.util.ArrayList;
-//import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Karen
 {
@@ -24,39 +24,39 @@ public class Karen
         return this.name;
     }//getter for Karen name
 
-//    public int getNumRacers()
-//    {
-//        return this.numRacers;
-//    }
+    public int getNumRacers()
+    {
+        return this.numRacers;
+    }
 
     public void setNumRacers(int numRacers)
     {
         this.numRacers = numRacers;
     }//setter for numRacers, the number of animals being raced
 
-//    public void createAnimal(String type, ArrayList list)
-//    {
-//        String typeCheck = type;
-//        Scanner input = new Scanner(System.in);
-//        switch (typeCheck)
-//        {
-//            case "d":
-//                System.out.println("What's the name of the dragon?\nEnter your dragon name (eg. 'toothless'):");
-//                Dragon creatureD = new Dragon(input.next());
-//                list.add(creatureD);
-//                break;
-//            case "u":
-//                System.out.println("What's the name of the unicorn?\nEnter your unicorn name (eg. 'rainbows'):");
-//                Unicorn creatureU = new Unicorn(input.next());
-//                list.add(creatureU);
-//                break;
-//            case "g":
-//                break;
-//            default:
-//                System.out.println("Enter from the given options:\n 'd' for dragon\n 'u' for unicorn\n 'g' for griffith");
-//                typeCheck = input.next();
-//        }
-//    }
+    public void createAnimal(String type, ArrayList list)
+    {
+        String typeCheck = type;
+        Scanner input = new Scanner(System.in);
+        switch (typeCheck)
+        {
+            case "d":
+                System.out.println("What's the name of the dragon?\nEnter your dragon name (eg. 'toothless'):");
+                Dragon creatureD = new Dragon(input.next());
+                list.add(creatureD);
+                break;
+            case "u":
+                System.out.println("What's the name of the unicorn?\nEnter your unicorn name (eg. 'rainbows'):");
+                Unicorn creatureU = new Unicorn(input.next());
+                list.add(creatureU);
+                break;
+            case "g":
+                break;
+            default:
+                System.out.println("Enter from the given options:\n 'd' for dragon\n 'u' for unicorn\n 'g' for griffith");
+                typeCheck = input.next();
+        }
+    }
 
 
 }
