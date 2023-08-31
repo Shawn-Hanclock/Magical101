de93bbe8e05935f68ac62204fdf7e9b9732b13b4
Shawn-Hanclock <nwahs007@gmail.com>
2023-08-31 13:00:24 -0700

    revised the user interaction in the prompts and code format

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,32 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Main
 {
     public static void main(String[] args)
     {
+        System.out.println("Hello new Karen, \nWhat is your name (eg. 'Duck'): ");
         Scanner input = new Scanner(System.in);
-        System.out.println("Hello new Karen, what is your name: ");
         Karen user = new Karen(input.next());
-        //take in user's name
-        System.out.println("Hello " + user.getName() + " we're located here at Magical McDonald's, How many creatures will you race?");
-        System.out.println("Just two? Or three? Enter '2' or '3': \n");
-        takeRacers(input);
+        //takes in user's name
+
+        System.out.println("Hello " + user.getName() + " we're located here at Magical McDonald's. \nHow many creatures will you put to race?\nEnter a whole number greater than 1 (eg. '2'):");
         user.setNumRacers(input.nextInt());
-        //take in number of racers
+        //takes the number of racers
 
-//        Dragon bob = new Dragon("bob", "lizard", 9999, 14.272);
-//        System.out.println(bob.toString());
-    }//end of main method
-
-    private static void takeRacers(Scanner value)
-    {
-        while(value.hasNextInt())
-        {
-            if (value.nextInt() != 2 || value.nextInt() != 3)
-            {
-                System.out.println("Just two? Or three? Enter '2' or '3': \n");
-            }
-        }//end of while
-    }//end of takeRacers
+//        ArrayList<MagicalAnimal> creatures = new ArrayList<MagicalAnimal>();
+//        System.out.println("Now lets pick out magical animals.");
+//        System.out.println("What should the first creature be?");
+//        boolean creaturesMade = false;
+//        while(!creaturesMade)
+//        {
+//            if(creatures.size() != 0)
+//                System.out.println("What should the next creature be?");
+//            System.out.println(" 'd' for dragon \n 'u' for unicorn \n 'g' for griffith");
+//            if(input.hasNext())
+//                user.createAnimal(input.next(), creatures);
+//            creaturesMade = creatures.size() == user.getNumRacers();
+//        }
+//        //creates arrayList of our magical animals
+//        System.out.println(creatures.get(0).toString());
+     }//end of main method
 }
