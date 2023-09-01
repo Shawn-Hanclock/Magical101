6887e699fac25bb9c369137fba6753fc3fd94f56
Shawn-Hanclock-Home <nwahs909@gmail.com>
2023-08-31 20:24:51 -0700

    added class specific instance variable

--- src/Dragon.java
+++ src/Dragon.java
@@ -1,21 +1,24 @@
 public class Dragon extends MagicalAnimal
 {
+    private int fireTemp;
     public Dragon()
     {
         super();
         this.type = "Dragon";
+        this.fireTemp = 0;
     }//empty constructor from magical animal
 
     public Dragon(String name)
     {
         super(name);
+        this.fireTemp = 1;
         this.type = "Dragon";
     }//full constructor from Magical animal
 
-    @Override
-    public String getName() {
-        return super.getName();
-    }//end of getter for Dragon name
+//    @Override
+//    public String getName() {
+//        return super.getName();
+//    }//end of getter for Dragon name
 
     @Override
     public String toString() {
